a2210997eb04c24cfb9fa04d630e8dcbd3b7ffa0
DonGoddard <Don@UFOStudios.net>
2026-02-18 13:04:40 -0500

    Fixed image loading, image state, drag

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "UFO Studios-NOS-0.7.260218.0749";
+const cacheName = "UFO Studios-NOS-0.7.260218.1256";
 const contentToCache = [
     "Build/XMLEditor.loader.js",
     "Build/XMLEditor.framework.js.unityweb",
